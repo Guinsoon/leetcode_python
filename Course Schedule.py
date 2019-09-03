10b8303dabdbfc9677d5eb3a7ac85d13f999f58d
Guinsoon <ywc1026@163.com>
2019-09-03 09:00:13 +0800

    daily

---
+++ Course Schedule.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses, prerequisites):
+        indegree = [0 for _ in range(numCourses)]
+        adj_list = {i: [] for i in range(numCourses)}
+
+        for link in prerequisites:
+            adj_list[link[1]].append(link[0])
+            indegree[link[0]] += 1
+
+        zero_degree = []
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                zero_degree.append(i)
+
+        i = 0
+        while i < len(zero_degree):
+            for node in adj_list[zero_degree[i]]:
+                indegree[node] -= 1
+                if indegree[node] == 0:
+                    zero_degree.append(node)
+            i += 1
+        if len(zero_degree) == numCourses:
+            return True
+        else:
+            return False
