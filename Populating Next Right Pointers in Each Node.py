69e0dee1cad187f639096f389431ee94b2f8717c
Guinsoon <ywc1026@163.com>
2019-09-07 11:00:10 +0800

    daily

---
+++ Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,24 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root):
+        if not root:
+            return
+        pre = root
+        cur = None
+        while pre.left:
+            cur = pre
+            while cur:
+                cur.left.next = cur.right
+                if cur.next:
+                    cur.right.next = cur.next.left
+                cur = cur.next
+            pre = pre.left
+        return root
