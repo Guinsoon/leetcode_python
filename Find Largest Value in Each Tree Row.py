f41deafdaa201d6b8e38a26cb8238c48d1566a8b
Guinsoon <ywc1026@163.com>
2019-09-13 20:44:37 +0800

    daily

--- Find Largest Value in Each Tree Row.py
+++ Find Largest Value in Each Tree Row.py
@@ -24,4 +24,29 @@ class Solution:
         self.dfs(root.left, res, level+1)
         self.dfs(root.right, res, level+1)
 
+    def largestValues2(self, root):
+        maxes = []
+        row = [root]
+        while any(row):
+            maxes.append(max(node.val for node in row))
+            row = [kid for node in row for kid in (node.left, node.right) if kid]
+        return maxes
+
+
+if __name__ == "__main__":
+    node1 = TreeNode(1)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    node5 = TreeNode(5)
+    node4 = TreeNode(3)
+    node9 = TreeNode(9)
+
+    node1.left = node3
+    node1.right = node2
+
+    node3.left = node5
+    node3.right = node4
+    node2.right = node9
+
+    print(Solution().largestValues2(node1))
 
