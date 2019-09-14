334f872cc6b10b9f39e046d892863ac4792c5dc6
Guinsoon <ywc1026@163.com>
2019-09-14 17:11:18 +0800

    daily

--- Combinations.py
+++ Combinations.py
@@ -1,4 +1,27 @@
 class Solution:
     def combine(self, n, k):
-        pass
-    
+        res = []
+        self.dfs2(res, n, k, [], 1)
+        return res
+
+    def dfs(self, res, n, k, out, start):
+        if k == 0:
+            res.append(out[:])
+            return
+        for i in range(start, n-k+2):
+            out.append(i)
+            self.dfs(res, n, k-1, out, i+1)
+            out.pop()
+
+    def dfs2(self, res, n, k, out, start):
+        if len(out) == k:
+            res.append(out[:])
+            return
+        for i in range(start, n+1):
+            out.append(i)
+            self.dfs2(res, n, k, out, i+1)
+            out.pop()
+
+
+if __name__ == "__main__":
+    print(Solution().combine(4, 2))
