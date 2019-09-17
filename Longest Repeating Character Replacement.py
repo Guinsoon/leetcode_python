4fb98b47a5331bd91962abb44a8c27f5597741aa
Guinsoon <ywc1026@163.com>
2019-09-17 09:13:12 +0800

    modify

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -3,3 +3,17 @@ from collections import Counter
 
 class Solution:
     def characterReplacement(self, s, k):
+        lo = hi = 0
+        counter = Counter()
+        for hi in range(len(s)):
+            counter[s[hi]] += 1
+            max_count = counter.most_common(1)[0][1]
+            if hi - lo - max_count + 1 > k:
+                counter[s[lo]] -= 1
+                lo += 1
+        return hi - lo + 1
+
+
+if __name__ == "__main__":
+    print(Solution().characterReplacement("ABAB", 2))
+
