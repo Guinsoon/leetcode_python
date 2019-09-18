5563b65f848a25749e95f2e10dc9bbb013a2c7a8
Guinsoon <ywc1026@163.com>
2019-09-18 10:03:01 +0800

    daily

---
+++ Binary Tree Pruning.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/9/18 上午10:02
+@desc: Bigo
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def pruneTree(self, root):
+        pass
