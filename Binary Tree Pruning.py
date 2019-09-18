20d6eba77165c0e2fd0d2d65e0b4d72a9f871dc6
Guinsoon <ywc1026@163.com>
2019-09-18 10:13:31 +0800

    daily

--- Binary Tree Pruning.py
+++ Binary Tree Pruning.py
@@ -4,6 +4,8 @@
 @time: 2019/9/18 上午10:02
 @desc: Bigo
 """
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -11,6 +13,27 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
     def pruneTree(self, root):
-        pass
+        if not root:
+            return
+        if root and not root.left and not root.right and root.val == 0:
+            return
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        return root
+
+
+if __name__ == "__main__":
+    node1 = TreeNode(1)
+    node2 = TreeNode(0)
+    node3 = TreeNode(0)
+    node4 = TreeNode(1)
+
+    node1.right = node2
+    node2.left = node3
+    node2.right = node4
+
+    print(Solution().pruneTree(node1))
+
