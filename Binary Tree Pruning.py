f039210d7fa436b44e3bd7e7bb253688a0472692
Guinsoon <ywc1026@163.com>
2019-09-18 10:19:31 +0800

    daily

--- Binary Tree Pruning.py
+++ Binary Tree Pruning.py
@@ -18,10 +18,10 @@ class Solution:
     def pruneTree(self, root):
         if not root:
             return
-        if root and not root.left and not root.right and root.val == 0:
-            return
         root.left = self.pruneTree(root.left)
         root.right = self.pruneTree(root.right)
+        if root and not root.left and not root.right and root.val == 0:
+            return
         return root
 
 
