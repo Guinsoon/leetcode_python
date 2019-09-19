025a84b76555ac2e834b279fa07971a88d24852b
Guinsoon <ywc1026@163.com>
2019-09-19 08:45:35 +0800

    daily

--- Insert Interval.py
+++ Insert Interval.py
@@ -4,23 +4,20 @@
 @time: 2019/9/19 上午8:26
 @desc: Bigo
 """
+
+
 class Solution:
     def insert(self, intervals, newInterval):
-        if not newInterval:
-            return intervals
-        if newInterval[1] < intervals[0][1]:
-            return [newInterval] + intervals
-        if newInterval[0] > intervals[-1][0]:
-            return intervals + [newInterval]
+        low = 0
+        high = 0
+        for item in intervals:
+            if item[0] <= newInterval[0] <= item[1]:
+                low = intervals.index(item)
+            if item[0] <= newInterval[1] <= item[1]:
+                high = intervals.index(item)
+        min_item = min(newInterval[0], intervals[low[0]])
+        max_item = max(newInterval[1], intervals[high[1]])
+
         res = []
         for item in intervals:
-            if item[1] < newInterval[0]:
-                res.append(item)
-            elif item[0] > newInterval[1]:
-                if newInterval not in res:
-                    res.append(newInterval)
-                res.append(item)
-            elif item[0] < newInterval[0] < item[1]:
-
-
-
+            if
