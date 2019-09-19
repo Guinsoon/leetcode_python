435d6b46154b624c0be6e02d55a3a90701a1ebfe
Guinsoon <ywc1026@163.com>
2019-09-19 09:26:08 +0800

    daily

--- Insert Interval.py
+++ Insert Interval.py
@@ -8,44 +8,7 @@
 
 class Solution:
     def insert(self, intervals, newInterval):
-        if not intervals:
-            return [newInterval]
-        if newInterval[1] < intervals[0][0]:
-            return [newInterval] + intervals
-        if newInterval[0] > intervals[-1][1]:
-            return intervals + [newInterval]
-        # if newInterval[0] < intervals[0][0] and newInterval[1] > intervals[-1][1]:
-        #     return [newInterval]
-        low = -1
-        high = len(intervals)
-        for item in intervals:
-            if item[0] <= newInterval[0] <= item[1]:
-                low = intervals.index(item)
-            if item[0] <= newInterval[1] <= item[1]:
-                high = intervals.index(item)
-        min_item = 0
-        max_item = 0
-        if 0 <= low <= len(intervals) and 0 <= high <= len(intervals):
-            min_item = min(newInterval[0], intervals[low][0])
-            max_item = max(newInterval[1], intervals[high][1])
-        else:
-            if low == -1:
-                min_item = newInterval[0]
-            if high == len(intervals):
-                max_item = newInterval[1]
 
-        res = []
-        i = 0
-        while i < len(intervals):
-            if i < low or i > high:
-                res.append(intervals[i])
-                i += 1
-            else:
-                if i == low:
-                    res.append([min_item, max_item])
-                while i <= high:
-                    i += 1
-        return res
 
 
 if __name__ == "__main__":
