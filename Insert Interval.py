687fb3f3bf92b5262b2924c12927b6ac14adedba
Guinsoon <ywc1026@163.com>
2019-09-19 08:35:28 +0800

    daily

--- Insert Interval.py
+++ Insert Interval.py
@@ -8,5 +8,19 @@ class Solution:
     def insert(self, intervals, newInterval):
         if not newInterval:
             return intervals
+        if newInterval[1] < intervals[0][1]:
+            return [newInterval] + intervals
+        if newInterval[0] > intervals[-1][0]:
+            return intervals + [newInterval]
         res = []
         for item in intervals:
+            if item[1] < newInterval[0]:
+                res.append(item)
+            elif item[0] > newInterval[1]:
+                if newInterval not in res:
+                    res.append(newInterval)
+                res.append(item)
+            elif item[0] < newInterval[0] < item[1]:
+
+
+
