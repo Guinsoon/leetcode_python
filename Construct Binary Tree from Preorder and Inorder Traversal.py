2e70ef0654f39e0dbf62147c15da135176d95095
Guinsoon <ywc1026@163.com>
2019-09-20 21:04:16 +0800

    daily

---
+++ Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/9/20 下午9:03
+@desc: Bigo
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
