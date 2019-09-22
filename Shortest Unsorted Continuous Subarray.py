683c6d8834a1cfe04550a6fb3210732d9cbd1a5d
Guinsoon <ywc1026@163.com>
2019-09-22 10:43:45 +0800

    daily

--- Shortest Unsorted Continuous Subarray.py
+++ Shortest Unsorted Continuous Subarray.py
@@ -7,4 +7,22 @@
 
 
 class Solution:
-    def findUnsortedSubarray(self, nums: List[int]) -> int:
+    def findUnsortedSubarray(self, nums):
+        if not nums:
+            return 0
+        start = None
+        end = None
+        for i in range(len(nums)-1):
+            if nums[i+1] < nums[i]:
+                if start is None:
+                    start = i
+                end = i + 1
+
+        if start is None:
+            return len(nums)
+        else:
+            return end-start+1
+
+i
+
+
