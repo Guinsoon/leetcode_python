aa2894c3d4fd8497894821c77b7b48d0cba19ad1
Guinsoon <ywc1026@163.com>
2019-09-23 09:12:52 +0800

    daily

--- Sort Colors.py
+++ Sort Colors.py
@@ -11,3 +11,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        for i in range(1, len(nums)):
+            pre = i - 1
+            cur = nums[i]
+            while pre >= 0 and cur < nums[pre]:
+                nums[pre + 1] = nums[pre]
+                pre -= 1
+            nums[pre + 1] = cur
+
+
+if __name__ == "__main__":
+    a = [2, 0, 2, 1, 1, 0]
+    print(Solution().sortColors(a))
