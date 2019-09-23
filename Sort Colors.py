70e06ffaea65a829e4cedc7a25e1e0e8b5af26c1
Guinsoon <ywc1026@163.com>
2019-09-23 08:44:26 +0800

    daily

---
+++ Sort Colors.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/9/23 上午8:44
+@desc: Bigo
+"""
+
+
+class Solution:
+    def sortColors(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
