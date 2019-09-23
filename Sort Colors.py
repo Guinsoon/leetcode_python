ddc5d8660e0510fcc39e941ec119b8e242ad46c2
Guinsoon <ywc1026@163.com>
2019-09-23 09:28:12 +0800

    daily

--- Sort Colors.py
+++ Sort Colors.py
@@ -19,7 +19,25 @@ class Solution:
                 pre -= 1
             nums[pre + 1] = cur
 
+    def dutch_flag(self, nums):
+        red, white, blue = 0, 0, len(nums)-1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+
+            elif nums[white] == 1:
+                white += 1
+
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+
 
 if __name__ == "__main__":
     a = [2, 0, 2, 1, 1, 0]
-    print(Solution().sortColors(a))
+    Solution().sortColors(a)
+    print(a)
+
