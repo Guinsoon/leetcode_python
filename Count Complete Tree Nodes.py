5b5bb32a868382553bdfeb1494ed0157add87dfd
Guinsoon <ywc1026@163.com>
2019-09-24 09:27:13 +0800

    daily

--- Count Complete Tree Nodes.py
+++ Count Complete Tree Nodes.py
@@ -15,3 +15,20 @@ class TreeNode:
 
 class Solution:
     def countNodes(self, root):
+        """
+        bfs
+        :param root:
+        :return:
+        """
+        if not root:
+            return 0
+        queue = [root]
+        res = []
+        while queue:
+            node = queue.pop(0)
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return len(res)
