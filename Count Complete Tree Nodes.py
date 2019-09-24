c53fcd6970fc94ce80a8bb3bee28271b81bcca9c
Guinsoon <ywc1026@163.com>
2019-09-24 09:23:42 +0800

    daily

---
+++ Count Complete Tree Nodes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/9/24 上午9:23
+@desc: Bigo
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def countNodes(self, root):
