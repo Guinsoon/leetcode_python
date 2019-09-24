33469ab33ef098ab1b839f78b64ac95448ff5b56
Guinsoon <ywc1026@163.com>
2019-09-24 09:41:42 +0800

    daily

--- Count Complete Tree Nodes.py
+++ Count Complete Tree Nodes.py
@@ -13,6 +13,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
     def countNodes(self, root):
         """
@@ -32,3 +33,26 @@ class Solution:
             if node.right:
                 queue.append(node.right)
         return len(res)
+
+    def countNodes2(self, root):
+        """
+        dfs
+        compare the depth between left sub tree and right sub tree.
+        A, If it is equal, it means the left sub tree is a full binary tree
+        B, It it is not , it means the right sub tree is a full binary tree
+        :param root:
+        :return:
+        """
+        if not root:
+            return 0
+        left_depth = self.getDepth(root.left)
+        right_depth = self.getDepth(root.right)
+        if left_depth == right_depth:
+            return pow(2, left_depth) + self.countNodes2(root.right)
+        else:
+            return pow(2, right_depth) + self.countNodes2(root.left)
+
+    def getDepth(self, root):
+        if not root:
+            return 0
+        return 1 + self.getDepth(root.left)
