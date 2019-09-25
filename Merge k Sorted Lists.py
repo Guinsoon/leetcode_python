aae6d6971ee25eb8b6a121d9da8a742b9b23696c
Guinsoon <ywc1026@163.com>
2019-09-25 09:10:54 +0800

    daily

---
+++ Merge k Sorted Lists.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/9/25 上午9:08
+@desc: Bigo
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeKLists(self, lists):
+        pass
