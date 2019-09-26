024036ba9fd9e7876e1763fccb56a863e77a2cf8
Guinsoon <ywc1026@163.com>
2019-09-26 08:45:33 +0800

    daily

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -7,4 +7,17 @@
 
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindrome(self, s):
+        if not s:
+            return
+        is_same = [a == b for a, b in zip(s, s[::-1])]
+        res = ''
+        for i in range(len(is_same)):
+            if is_same[i] is True:
+                res += s[i]
+        return res
+
+
+if __name__ == "__main__":
+    s = 'babad'
+    print(Solution().longestPalindrome(s))
