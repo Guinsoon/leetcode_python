8aa1d43defb6f39633b2e3163c9252258825c9bd
Guinsoon <ywc1026@163.com>
2019-09-27 09:19:35 +0800

    daily

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -24,24 +24,11 @@ class Solution:
             r += 1
         return s[l+1: r]
 
-    def longestPalindrome2(self, s):
-        new_string = ""
-        for i in range(len(s)):
-            new_string += "#"
-            new_string += s[i]
-        new_string += "#"
-        res = ""
-        for i in range(len(s)):
-            temp = self.helper(s, i, i)
-            if len(temp) > len(res):
-                res = temp
-        return res
-
 
 if __name__ == "__main__":
     s = 'babad'
-    print(Solution().longestPalindrome2(s))
-    print(Solution().longestPalindrome2('ac'))
-    print(Solution().longestPalindrome2('abb'))
+    print(Solution().longestPalindrome(s))
+    print(Solution().longestPalindrome('ac'))
+    print(Solution().longestPalindrome('abb'))
 
 
