9fe85d5ed1a987f91a758c19124ba3002d2cd9ba
Guinsoon <ywc1026@163.com>
2019-09-28 21:53:17 +0800

    daily

--- Palindromic Substrings.py
+++ Palindromic Substrings.py
@@ -8,4 +8,22 @@
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        pass
+        res = []
+        self.dfs(s, res, "", 0)
+        return res
+
+    def dfs(self, s, res, temp, idx):
+        res.append(temp)
+        for i in range(idx, len(s)):
+            temp += s[i]
+            self.dfs(s, res, temp, idx+1)
+            temp = temp[:-1]
+
+    def is_palindromic(self, s):
+        return s == s[::-1]
+
+
+if __name__ == "__main__":
+    s = 'abc'
+    print(Solution().countSubstrings(s))
+
