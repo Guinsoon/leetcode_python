345e2598a436f1d6051347b75170c07ec08bf107
Guinsoon <ywc1026@163.com>
2019-09-29 09:12:18 +0800

    daily

--- Palindromic Substrings.py
+++ Palindromic Substrings.py
@@ -8,23 +8,17 @@
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        pass
+
+    def myself(self, s):
         if not s:
             return 1
-        res = []
-        out = -1
-        self.dfs(s, res, "", 0)
-        print(res)
-        for item in res:
-            if self.is_palindromic(item):
-                out += 1
-        return out
-
-    def dfs(self, s, res, temp, idx):
-        res.append(temp)
-        for i in range(idx, len(s)):
-            temp += s[i]
-            self.dfs(s, res, temp, i+1)
-            temp = temp[:-1]
+        res = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if self.is_palindromic(s[i:j+1]):
+                    res += 1
+        return res
 
     def is_palindromic(self, s):
         return s == s[::-1]
@@ -32,7 +26,7 @@ class Solution:
 
 if __name__ == "__main__":
     s = 'abc'
-    print(Solution().countSubstrings(s))
-    print(Solution().countSubstrings("aaa"))
+    print(Solution().myself(s))
+    print(Solution().myself("aaa"))
 
 
