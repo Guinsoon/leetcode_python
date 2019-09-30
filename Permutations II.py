3362a6a9c31e690ea2a5df72d40411964996e90f
Guinsoon <ywc1026@163.com>
2019-09-30 08:37:27 +0800

    daily

--- Permutations II.py
+++ Permutations II.py
@@ -7,5 +7,19 @@
 
 
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        pass
+    def permuteUnique(self, nums):
+        res = []
+        self.dfs(res, [], nums, 0)
+        return res
+
+    def dfs(self, res, temp, nums, idx):
+        if temp not in res:
+            res.append(temp[:])
+        for i in range(idx, len(nums)):
+            temp.append(nums[i])
+            self.dfs(res, temp, nums, i+1)
+            temp.pop()
+
+
+if __name__ == "__main__":
+    print(Solution().permuteUnique([1, 1, 2]))
