6a10a6c6688b638e4474ea741a5ef29077b6b39a
Guinsoon <ywc1026@163.com>
2019-10-01 14:50:12 +0800

    daily

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -9,5 +9,17 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs):
-        res = defaultdict(list)
+        out = defaultdict(list)
+        for item in strs:
+            k = "".join(sorted(item))
+            out[k].append(item)
+        res = []
+        for _, values in out.items():
+            res.append(values)
+        return res
+
+
+if __name__ == "__main__":
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    print(Solution().groupAnagrams(strs))
 
