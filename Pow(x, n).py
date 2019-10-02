83cda4e4eb3c06825601a5a8b70234e7ce283799
Guinsoon <ywc1026@163.com>
2019-10-02 09:33:35 +0800

    daily

--- Pow(x, n).py
+++ Pow(x, n).py
@@ -8,6 +8,12 @@
 
 class Solution:
     def myPow(self, x, n):
+        """
+        TLE
+        :param x:
+        :param n:
+        :return:
+        """
         if n == 0:
             return 1
         if n == 1:
@@ -16,8 +22,17 @@ class Solution:
             return self.myPow(1/x, -n)
         return self.myPow(x, n-1) * x
 
+    def myPow2(self, x, n):
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        return self.myPow2(x*x, n // 2) if n % 2 == 0 else x * self.myPow2(x*x, n//2)
+
 
 if __name__ == "__main__":
     print(Solution().myPow(2.1, 3))
     print(Solution().myPow(2, -2))
+    print(Solution().myPow2(2, -2))
 
