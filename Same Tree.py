b7b49ebef35650ee13f3cd8ac877a77cbfb1c4aa
Guinsoon <ywc1026@163.com>
2019-10-05 20:53:53 +0800

    daily

--- Same Tree.py
+++ Same Tree.py
@@ -16,23 +16,13 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p, q):
-        res_p = self.inorder(p)
-        res_q = self.inorder(q)
-        return res_p == res_q
-
-    def inorder(self, root):
-        if not root:
-            return
-        stack = []
-        res = []
-        while stack or root:
-            while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            res.append(root.val)
-            root = root.right
-        return res
+        if not p and not q:
+            return True
+        if not p and q:
+            return False
+        if p and not q:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
 
 if __name__ == "__main__":
@@ -43,4 +33,4 @@ if __name__ == "__main__":
     node1.left = node2
     node1.right = node3
 
-    print(Solution().inorder(node1))
+    print(Solution().isSameTree(node1, node2))
