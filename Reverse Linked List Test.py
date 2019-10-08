0a14d6ef0f9323cf8d973d55d6c225449859eabc
Guinsoon <ywc1026@163.com>
2019-10-08 08:59:09 +0800

    linked list

---
+++ Reverse Linked List Test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/10/8 上午8:58
+@desc: Bigo
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head):
+        pass
