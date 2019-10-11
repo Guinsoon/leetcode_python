b41d915999ba86b9cb1bd195f620cef322f0ef98
Guinsoon <ywc1026@163.com>
2019-10-11 08:49:18 +0800

    unique binary tree

---
+++ test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/10/11 上午8:45
+@desc: Bigo
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def generateTrees(self, n):
+        pass
+
+    def dfs(self, start, end):
+        pass
