2f660a31376434017453c53ab502f3b8c171a9df
Guinsoon <ywc1026@163.com>
2019-10-14 14:10:43 +0800

    coding interviews

---
+++ coding_interviews/replace_space.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/10/14 上午10:02
+@desc: Bigo
+"""
+
+
+class Solution:
+    def replaceSpace(self, s):
+        for item in s:
+            if item == " ":
+                pass
