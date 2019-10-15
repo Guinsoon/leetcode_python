e5122ea5dfcd7ee3fbb9ecea6253e5be350e1e0e
Guinsoon <ywc1026@163.com>
2019-10-15 08:33:37 +0800

    coding interviews

--- coding_interviews/find_in_matrix.py
+++ coding_interviews/find_in_matrix.py
@@ -8,4 +8,23 @@
 
 class Solution:
     def Find(self, target, array):
-        pass
+        if not target:
+            return False
+        r = len(array)
+        c = len(array[0])
+        i = 0
+        j = c - 1
+        while i < r and j >= 0:
+            if target == array[i][j]:
+                return True
+            elif target < array[i][j]:
+                j -= 1
+            elif target > array[i][j]:
+                i += 1
+        return False
+
+
+if __name__ == "__main__":
+    nums = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
+    item = 7
+    print(Solution().Find(item, nums))
