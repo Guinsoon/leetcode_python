802b2ed55451851c0f3d9d080a68fd2ed1948b0c
Guinsoon <ywc1026@163.com>
2019-10-15 08:44:29 +0800

    coding interviews

---
+++ coding_interviews/print_linkedlist.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Nicotine
+@time: 2019/10/15 上午8:43
+@desc: Bigo
+"""
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def printListFromTailToHead(self, listNode):
+        pass
